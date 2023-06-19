157922a1b5a20a0dc1f54ff41667976560cbba25
jack <gangchengcn@qq.com>
2023-06-19 11:10:13 +0800

    排序/连接redis

--- std/redis_go.go
+++ std/redis_go.go
@@ -5,20 +5,19 @@ import (
 	"github.com/go-redis/redis"
 )
 
-var redisclient *redis.Client
+var redisClient *redis.Client
 
 func initRedis() (err error) {
-	redisclient = redis.NewClient(&redis.Options{
+	redisClient = redis.NewClient(&redis.Options{
 		Addr:     "112.126.71.240:6379",
 		Password: "dockerredis", //
 		DB:       0,
 	})
-	_, err = redisclient.Ping().Result()
+	_, err = redisClient.Ping().Result()
 	return
 }
 
 // go 连接redis
-
 func RedisBasic() {
 	err := initRedis()
 	if err != nil {
@@ -26,7 +25,7 @@ func RedisBasic() {
 		return
 	}
 	fmt.Println("redis连接成功！")
-	s, err := redisclient.Get("login:token:u7fpffqgduw17y3eh5zomltaunfockaz").Result()
+	s, err := redisClient.Get("login:token:u7fpffqgduw17y3eh5zomltaunfockaz").Result()
 	fmt.Println(s)
 
 	defer func(redisclient *redis.Client) {
@@ -34,5 +33,5 @@ func RedisBasic() {
 		if err != nil {
 			panic(err)
 		}
-	}(redisclient)
+	}(redisClient)
 }
