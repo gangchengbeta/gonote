3ef27749e3bd53a2745a11bf44841b30d7a52b48
jack <gangchengcn@qq.com>
2023-06-20 18:35:15 +0800

    读写文件优化

--- std/file_util.go
+++ std/file_util.go
@@ -1,7 +1,9 @@
 package std
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
@@ -36,7 +38,7 @@ func createDir(path string) error {
 func FileOperation() {
 	//dir := readDir("D:\\CodeProjects")
 	//fmt.Println(dir)
-	fileRW("d1.txt")
+	fileRWBuf("d1.txt")
 }
 
 // 读取文件夹下的所有文件
@@ -92,4 +94,48 @@ func fileRW(path string) {
 	//if err != nil {
 	//	panic(err)
 	//}
+}
+func fileRWBuf(path string) {
+	// 打开文件
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	// 延时关闭文件 (必须有)
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
+
+	//使用缓冲区读取文件 减少磁盘IO提高程序性能
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n') // 碰到换行符就结束读取
+
+		if err != nil {
+			// 判断是否读取到文件末尾
+			if err == io.EOF {
+				fmt.Println(line)
+				fmt.Println("文件读取完毕")
+				break
+			}
+			panic(err)
+		}
+		fmt.Print(line)
+	}
+
+	//使用缓冲区写入文件 减少磁盘IO提高程序性能
+	writer := bufio.NewWriter(file)
+	_, err = writer.WriteString("hello world")
+	if err != nil {
+		panic(err)
+	}
+	//刷新缓冲区
+	err = writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+
 }
