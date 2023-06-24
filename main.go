2ab6e63cd5fb5ea1178679598527bf8d4190d45c
gangchengbeta <gangchengcn@qq.com>
2023-06-24 11:53:46 +0800

    gorm操作redis

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	myclient "gonote/gin"
+)
+
 // 主函数
 func main() {
-
+	engine := gin.Default()
+	engine.GET("/get_name", myclient.GetName)
+	err := engine.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
 }
